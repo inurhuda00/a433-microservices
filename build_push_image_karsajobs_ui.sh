64b2ebd180de265ed1ca777c70267c8ccf3eb016
inurhuda00 <inurhuda00@gmail.com>
2023-10-17 21:25:12 +0700

    script push & workflow

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Nama image yang akan dibuat
+IMAGE_NAME="karsajob-ui"
+# Versi image (tag)
+IMAGE_TAG="latest"
+# Nama akun GitHub 
+GITHUB_USERNAME="inurhuda00"
+
+# export CR_PAT=YOUR_TOKEN; 
+echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
+
+# Perintah untuk membuat Docker image dari Dockerfile
+docker build -t $IMAGE_NAME:$IMAGE_TAG -f Dockerfile .
+
+# Melihat daftar image di lokal
+docker images
+
+# Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag $IMAGE_NAME:$IMAGE_TAG  ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG
+
+# Mengunggah image ke GitHub Packages
+docker push  ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG
